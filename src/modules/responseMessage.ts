fb1b6f444ef6d99ae6d4ba937ce8010b6bf5ecf3
= <tmdgk490255@gmail.com>
2022-05-21 23:44:51 +0900

    [ADD] responseMessage 추가

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -2,7 +2,16 @@ const message = {
     NULL_VALUE: '필요한 값이 없습니다.',
     NOT_FOUND: '존재하지 않는 자원',
     BAD_REQUEST: '잘못된 요청',
-    INTERNAL_SERVER_ERROR: '서버 내부 오류'
+    INTERNAL_SERVER_ERROR: '서버 내부 오류',
+
+    // board
+    CREATE_BOARD_SUCCESS: '글 생성 성공',
+    READ_BOARD_SUCCESS: '글 조회 성공',
+    READ_BOARDS_SUCCESS: '글 리스트 조회 성공',
+
+    CREATE_BOARD_FAIL: '글 생성 실패',
+    READ_BOARD_FAIL: '글 조회 실패',
+    READ_BOARDS_FAIL: '글 리스트 조회 실패',
 }
 
 export default message;
